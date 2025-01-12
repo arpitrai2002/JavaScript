12d0ba9e60a7de644a76e1e355286619298f34f1
Arpit Rai <arpitr2002@outlook.com>
2025-01-12 13:14:41 +0530

    Math

--- Basics/7Number_Maths.js
+++ Basics/7Number_Maths.js
@@ -26,5 +26,35 @@ const a=balance.valueOf();  // A number representing the primitive value of the 
 
 console.log(a);
 
+Number.MAX_VALUE;
+Number.MIN_VALUE;
+
 
 // ******************************* Maths*****************************************
+console.log(Math);
+console.log(Math.PI);
+
+console.log(Math.abs(-5));
+console.log(Math.round(4.5));
+console.log(Math.ceil(4.1));
+console.log(Math.floor(4.9));
+
+console.log(Math.max(4,5,9,3,7,4,1));
+console.log(Math.min(4,5,9,3,7,4,1));
+
+
+console.log(Math.random())
+console.log(Math.random()*10);
+console.log(Math.floor(Math.random()*10)+1);
+
+
+
+// Dice problem
+let min=1;
+let max=6;
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
+
