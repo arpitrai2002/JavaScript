4916235762f3717cdb89903e16a325a97877b1f6
Arpit Rai <arpitr2002@outlook.com>
2025-01-12 11:27:37 +0530

    String

---
+++ Basics/6string.js
@@ -0,0 +1,10 @@
+let name="Arpit";
+let Age=22;
+
+console.log(name+" "+Age);
+
+console.log(`My name is ${name} and age is ${Age}`);
+
+let newString=new String("Arpit Kumar Rai");
+console.log(newString.length);
+console.log(newString.toUpperCase());
